6bf79e078460297c42171d0a3f35f241b6b98dc8
andershaig <andershaig@gmail.com>
2011-08-27 21:40:10 -0500

    Started project template

--- js/global.js
+++ js/global.js
@@ -1,14 +1,5 @@
 // Start 
 
-//$(document).ready( function(){ simpleSlide(); });
-
-// Fade links
-
-$(function () {
-	$('.fade').hover(function() {
-		$(this).fadeTo("fast", 1);
-	}, function() {
-		$(this).fadeTo("75", 0.5);
-	});
-
-});
+//$(document).ready( function(){
+//	
+//});
