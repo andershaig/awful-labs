9fb7eeb2bac323ff7f78221a7de1d16e97554e2c
andershaig <andershaig@gmail.com>
2011-10-22 00:21:06 -0700

    Started fixing up the Awful Bar for use

---
+++ projects/backgrounds/js/global.js
@@ -0,0 +1,16 @@
+// Start
+
+$(document).ready(
+	
+);
+
+// Fade links
+
+$(function () {
+	$('.fade').hover(function() {
+		$(this).fadeTo("fast", 1);
+	}, function() {
+		$(this).fadeTo("75", 0.5);
+	});
+
+});
