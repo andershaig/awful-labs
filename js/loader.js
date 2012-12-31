a2090b875099c34c2f3a65d9a0fb028dc44500bc
Anders Haig <andershaig@gmail.com>
2012-12-31 12:58:49 -0800

    Reorg and add nav bar loader

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+$(document).ready( function () {
+	// Load Nav Bar
+	var awful_bar =  '<div id="awful_nav" class="clearfix">';
+		awful_bar += '<span id="title"><a href="/">Awful Labs</a></span>';
+		awful_bar += '<span id="subtitle"><b>aw &middot; ful</b> <i>(adjective)</i> solemnly impressive; inspiring awe; full of awe;</span>';	
+		awful_bar += '</div>';
+
+	$('#awful_bar').html(awful_bar);
+});
